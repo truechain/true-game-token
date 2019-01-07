74496883c1efc72033d760b1683ba958cf1935df
tari404 <2680383605@qq.com>
2019-01-07 17:37:18 +0800

    ⚙️ initialize contracts and exchange services

---
+++ server/index.js
@@ -0,0 +1,161 @@
+const Web3 = require('web3')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const trueTokenABI = require('../sol/TrueToken.abi.json')
+const TGTokenABI = require('../sol/TrueGameToken.abi.json')
+
+const config = require('./config.json')
+
+const tWeb3 = new Web3('wss://api.truescan.net/ws')
+// const eWeb3 = new Web3('https://mainnet.infura.io', 'eth')
+const eWeb3 = new Web3('https://ropsten.infura.io', 'eth') // test eth net
+// const ethTrueAddress = '0xa4d17ab1ee0efdd23edc2869e7ba96b89eecf9ab'
+const ethTrueAddress = '0x2792d677B7Ba6B7072bd2293F64BC0C1CDe23ac1'.toLowerCase() // test eth net
+const betaTGTAddress = '0x912dEb842f3CE780c7A8e0B8e74e42E87C575D97'
+const privKey = config.privKey
+const topic = tWeb3.eth.abi.encodeEventSignature('SendOut(uint256,address,uint256)')
+
+const trueToken = new eWeb3.eth.Contract(trueTokenABI, ethTrueAddress)
+const TGToken = new tWeb3.eth.Contract(TGTokenABI, betaTGTAddress)
+const admin = eWeb3.eth.accounts.privateKeyToAccount(privKey)
+eWeb3.eth.accounts.wallet.add(admin)
+tWeb3.eth.accounts.wallet.add(admin)
+
+trueToken.methods.name().call().then(name => {
+  console.log(`---- Successful link to ${name} contract on ETH`)
+})
+
+let sub
+
+function init () {
+  console.log('...link to ws service')
+  tWeb3.setProvider('wss://api.truescan.net/ws')
+  const ws = tWeb3.currentProvider.connection
+  ws.onopen = () => {
+    if (sub) {
+      sub.unsubscribe()
+    }
+    sub = tWeb3.eth.subscribe('logs', {
+      address: betaTGTAddress,
+      topics: [topic]
+    }, (err, log) => {
+      if (err) {
+        console.log(err.message || err)
+      } else {
+        const logID = tWeb3.utils.hexToNumberString(log.topics[1])
+        const address = '0x' + log.topics[2].substr(26, 40)
+        const value = tWeb3.utils.hexToNumberString(log.data)
+        console.log('--------------------------------')
+        console.log('id:      ' + logID)
+        console.log('address: ' + address)
+        console.log('value:   ' + value)
+        console.log('--------------------------------')
+        sendTrueToken(logID, address, value)
+      }
+    })
+  }
+  ws.onclose = () => {
+    setTimeout(init, 2000)
+  }
+}
+init()
+
+setInterval(() => {
+  tWeb3.eth.net.getId().catch(() => {
+    console.log('---- ping error')
+  })
+}, 20000)
+
+function sendTrueToken (logID, address, value) {
+  trueToken.methods.transfer(address, value).send({
+    from: admin.address,
+    gas: '200000',
+    gasPrice: '10000000000'
+  }).on('transactionHash', hash => {
+    TGToken.methods.updateOutLog(logID, hash).send({
+      from: admin.address,
+      gas: '4000000',
+      gasPrice: '1'
+    })
+    console.log('> set hash: ' + hash + ' to log: ' + logID)
+  }).on('error'), error => {
+    console.log(error.message || error)
+  }
+}
+
+const app = express()
+app.use(bodyParser.json({ limit: '1mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.all('*', (_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  res.header('X-Powered-By', ' 3.2.1')
+  next()
+})
+app.get('/', (_, res) => {
+  res.send('hello world')
+})
+const pendingTxList = new Set()
+app.post('/', async (req, res) => {
+  const data = req.body
+  const hash = data.hash.toLowerCase()
+  if (!/^0x[0-9a-f]{64}$/.test(hash)) {
+    res.send(400, 'hash is a necessary parameter')
+  }
+  pendingTxList.add(hash)
+  res.send('ok')
+})
+
+app.listen(config.port)
+
+const ethTopic = eWeb3.eth.abi.encodeEventSignature('Transfer(address,address,uint256)')
+const addressTo = '0x000000000000000000000000' + admin.address.substr(2, 40).toLowerCase()
+async function checkTxHash (hash, removeList) {
+  const receipt = await eWeb3.eth.getTransactionReceipt(hash)
+  if (!receipt) {
+    return
+  }
+  removeList.push(hash)
+  if (receipt.to.toLowerCase() !== ethTrueAddress || !receipt.status || !receipt.logs[0]) {
+    return
+  }
+  const topics = receipt.logs[0].topics
+  if (topics[0] !== ethTopic || topics[2] !== addressTo) {
+    return
+  }
+  const { owner } = await TGToken.methods.inlogByTxHash(hash).call()
+  if (owner !== '0x0000000000000000000000000000000000000000') {
+    return
+  }
+  const from = '0x' + topics[1].substr(26, 40)
+  const value = tWeb3.utils.hexToNumberString(receipt.logs[0].data)
+  console.log(`[sendIn] to ${from} - ${value}`)
+  TGToken.methods.sendIn(from, value, hash).send({
+    from: admin.address,
+    gas: '4000000',
+    gasPrice: '1'
+  })
+}
+function checkTxList () {
+  const before = pendingTxList.size
+  const removeList = []
+  const promiseList = []
+  pendingTxList.forEach(hash => {
+    promiseList.push(checkTxHash(hash, removeList))
+  })
+  Promise.all(promiseList).catch(err => {
+    console.log(err.message || err)
+  }).then(() => {
+    removeList.forEach(hash => pendingTxList.delete(hash))
+    if (before) {
+      console.log(`>> pending tx: ${before} -> ${pendingTxList.size}`)
+    }
+    setTimeout(checkTxList, 10000)
+  })
+}
+checkTxList()
+
+console.log('---- Service start')
---
+++ sol/TrueGameToken.sol
@@ -0,0 +1,175 @@
+pragma solidity ^0.5.0;
+
+library SafeMath {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+  }
+}
+
+contract TrueGameToken {
+  using SafeMath for uint256;
+
+  struct Log {
+    address user;
+    uint256 value;
+    bytes32 txHash;
+  }
+
+  string public constant name = "True Game Token";
+  string public constant symbol = "TGT";
+  uint256 public constant decimals = 18;
+  uint256 private _totalSupply;
+  address payable public founder;
+  uint256 public charge;
+
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) _allowed;
+
+  mapping (bytes32 => Log) private _inLog;
+  mapping (address => bytes32[]) private _usersInLog;
+  Log[] private _outLog;
+  mapping (address => uint256[]) private _usersOutLog;
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  event SendOut(uint256 indexed _logID, address indexed _owner, uint256 _value);
+
+  constructor() public { 
+    founder = msg.sender;
+  }
+
+  function setCharge (uint256 _charge) public {
+    require(msg.sender == founder);
+    charge = _charge;
+  }
+
+  function sendIn(address _to, uint256 _amount, bytes32 _txHash) public {
+    require(msg.sender == founder);
+    require(_inLog[_txHash].user == address(0));
+
+    _totalSupply = _totalSupply.add(_amount);
+    _balances[_to] = _balances[_to].add(_amount);
+
+    _inLog[_txHash] = Log({
+      user: _to,
+      value: _amount,
+      txHash: _txHash
+    });
+    _usersInLog[_to].push(_txHash);
+  }
+
+  function sendOut(uint256 _value) public {
+    require(_value > charge);
+    require(_balances[msg.sender] >= _value);
+    _value -= charge;
+    _balances[msg.sender] -= _value;
+    _totalSupply -= _value;
+
+    uint256 index = _outLog.length;
+    _outLog.push(Log({
+      user: msg.sender,
+      value: _value,
+      txHash: bytes32(0)
+    }));
+    _usersOutLog[msg.sender].push(index);
+
+    emit SendOut(index, msg.sender, _value);
+  }
+
+  function updateOutLog (uint256 _logID, bytes32 _hash) public {
+    require(msg.sender == founder);
+    _outLog[_logID].txHash = _hash;
+  }
+
+  function inlogByTxHash(bytes32 _hash) public view returns (address owner, uint256 value) {
+    Log storage log = _inLog[_hash];
+    return (log.user, log.value);
+  }
+
+  function inLogCount (address _user) public view returns (uint256 count) {
+    return _usersInLog[_user].length;
+  }
+  function inLogPaged (address _user, uint256 _page, uint256 _size) public view returns (
+    uint256[] memory value,
+    bytes32[] memory txHash
+  ) {
+    require(_size <= 50);
+    value = new uint256[](_size);
+    txHash = new bytes32[](_size);
+    bytes32[] storage usersLog = _usersInLog[_user];
+    uint256 start = _page * _size;
+    for (uint256 i = start; i < usersLog.length; i++) {
+      Log storage log = _inLog[usersLog[i]];
+      value[i - start] = log.value;
+      txHash[i - start] = log.txHash;
+    }
+  }
+  
+  function outLogCount (address _user) public view returns (uint256 count) {
+    return _usersOutLog[_user].length;
+  }
+  function outLogPaged (address _user, uint256 _page, uint256 _size) public view returns (
+    uint256[] memory value,
+    bytes32[] memory txHash
+  ) {
+    require(_size <= 50);
+    value = new uint256[](_size);
+    txHash = new bytes32[](_size);
+    uint256[] storage usersLog = _usersOutLog[_user];
+    uint256 start = _page * _size;
+    for (uint256 i = start; i < usersLog.length; i++) {
+      Log storage log = _outLog[usersLog[i]];
+      value[i - start] = log.value;
+      txHash[i - start] = log.txHash;
+    }
+  }
+
+  function totalSupply() public view returns (uint256 supply) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return _balances[_owner];
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require((_balances[msg.sender] >= _value));
+    _balances[msg.sender] -= _value;
+    _balances[_to] = _balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_balances[_from] >= _value && _allowed[_from][msg.sender] >= _value);
+    _balances[_from] -= _value;
+    _allowed[_from][msg.sender] -= _value;
+    _balances[_to] = _balances[_to].add(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    _allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    return _allowed[_owner][_spender];
+  }
+
+  function changeFounder(address payable newFounder) public {
+    require(msg.sender == founder);
+
+    founder = newFounder;
+  }
+
+  function kill() public {
+    require(msg.sender == founder);
+
+    selfdestruct(founder);
+  }
+}
